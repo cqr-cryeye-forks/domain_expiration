26c114330b95f067ac2b00032fc1e4f32635fb19
anastasiia <stas.stasov.10398@gmail.com>
2023-05-19 18:16:46 +0300

    show first date only

--- domain_expiration.py
+++ domain_expiration.py
@@ -128,11 +128,11 @@ class DomainChecker(object):
             "name_servers": process_values_rm_symbols(whois_info.name_servers, r"\s*\r?\n\s*"),
             'registrar': process_values_rm_symbols(whois_info.registrar, r"\s+d/b/a\s+"),
 
-            'expiration_date': process_creation_dates(whois_info.expiration_date),
+            'expiration_date': process_creation_dates(whois_info.expiration_date[0]),
             'expired': check_expired < datetime.now(),
             'expire_soon': (check_expired - datetime.now()).days <= DEFAULT_DAYS_EXPIRATION,
-            'creation_date': process_creation_dates(whois_info.creation_date),
-            'updated_dates': process_creation_dates(whois_info.updated_date),
+            'creation_date': process_creation_dates(whois_info.creation_date)[0],
+            'updated_date': process_creation_dates(whois_info.updated_date)[0],
 
             'emails': process_values(whois_info.emails),
 
